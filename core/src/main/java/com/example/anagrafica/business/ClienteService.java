b29d98e05e9a483105513b94604f083eb2835d51
Domitilla Taxis <domitaxis@hotmail.it>
2019-10-29 17:37:50 +0100

    Update ClienteService con condizione cf univoco

--- core/src/main/java/com/example/anagrafica/business/ClienteService.java
+++ core/src/main/java/com/example/anagrafica/business/ClienteService.java
@@ -12,23 +12,28 @@ import com.example.anagrafica.data.IndirizzoCliente;
 import com.example.anagrafica.data.IndirizzoClienteRepository;
 
 @Service
-public class ClienteService{
+public class ClienteService {
 	@Autowired
 	ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	IndirizzoClienteRepository indirizzoClienteRepository;
 
-	public Boolean create(Cliente cliente, Map<String,Indirizzo> indirizzi) {
-		Cliente clienteSaved=clienteRepository.save(cliente);
-		IndirizzoCliente indirizzoCliente = null;
-		
-		for (String s: indirizzi.keySet()) {
-			indirizzoCliente=new IndirizzoCliente(indirizzi.get(s),clienteSaved,s);
-			indirizzoClienteRepository.save(indirizzoCliente);
+	public Boolean create(Cliente cliente, Map<String, Indirizzo> indirizzi) {
+		if (this.clienteRepository.existsByCf(cliente.getCf())) {
+			return null;
+		} else
+
+		{
+			Cliente clienteSaved = clienteRepository.save(cliente);
+			IndirizzoCliente indirizzoCliente = null;
+
+			for (String s : indirizzi.keySet()) {
+				indirizzoCliente = new IndirizzoCliente(indirizzi.get(s), clienteSaved, s);
+				indirizzoClienteRepository.save(indirizzoCliente);
+			}
+			return indirizzoCliente != null;
 		}
-		return indirizzoCliente !=null;
 	}
 
-	
 }
