38218b85ec4840eb7c9ff4ca6eb4455c89ac32cd
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-10-29 16:35:44 +0100

    Added data.sql file.

---
+++ core/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into indirizzo(luogo, numero_civico, citta, provincia, regione, nazione)
+values ("Via Marrone", 2, "Milano", "MI", "Lombardia", "Italia");
+
+insert into cliente(nome, cognome, sesso, cf, data_di_nascita, luogo_di_nascita, mail, telefono)
+values("Leonardo", "Mosciatti", "M", "ABCDEF123CIAO", "1996-1-1", "Siena", "leonardo@leonardo.com", "+393334455221");
+
