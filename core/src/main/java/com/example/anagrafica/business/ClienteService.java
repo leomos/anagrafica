28152083dde17c6371a3327d4f1a7429150f0a48
Domitilla Taxis <domitaxis@hotmail.it>
2019-10-29 16:54:51 +0100

    Added ClienteService.

---
+++ core/src/main/java/com/example/anagrafica/business/ClienteService.java
@@ -0,0 +1,34 @@
+package com.example.anagrafica.business;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.anagrafica.data.Cliente;
+import com.example.anagrafica.data.ClienteRepository;
+import com.example.anagrafica.data.Indirizzo;
+import com.example.anagrafica.data.IndirizzoCliente;
+import com.example.anagrafica.data.IndirizzoClienteRepository;
+
+@Service
+public class ClienteService{
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	IndirizzoClienteRepository indirizzoClienteRepository;
+
+	public Boolean create(Cliente cliente, Map<String,Indirizzo> indirizzi) {
+		Cliente clienteSaved=clienteRepository.save(cliente);
+		IndirizzoCliente indirizzoCliente = null;
+		
+		for (String s: indirizzi.keySet()) {
+			indirizzoCliente=new IndirizzoCliente(indirizzi.get(s),clienteSaved,s);
+			indirizzoClienteRepository.save(indirizzoCliente);
+		}
+		return indirizzoCliente !=null;
+	}
+
+	
+}
