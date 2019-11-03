968cad1b85fc0915798d0e95a0eccceb76dd7779
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-11-03 20:51:12 +0100

    Removed check constraint, seems like the syntax is wrong.

--- core/src/main/resources/data.sql
+++ core/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into indirizzo(luogo, numero_civico, citta, provincia, regione, nazione)
-values ("Via Marrone", 2, "Milano", "MI", "Lombardia", "Italia");
+values ('Via Marrone', 2, 'Milano', 'MI', 'Lombardia', 'Italia');
 
 insert into cliente(nome, cognome, sesso, cf, data_di_nascita, luogo_di_nascita, mail, telefono)
-values("Leonardo", "Mosciatti", "M", "ABCDEF123CIAO", "1996-1-1", "Siena", "leonardo@leonardo.com", "+393334455221");
+values('Leonardo', 'Mosciatti', 'M', 'ABCDEF123CIAO', '1996-1-1', 'Siena', 'leonardo@leonardo.com', '+393334455221');
 
-insert into indirizzo_cliente values (1,1,"residenza");
+insert into indirizzo_cliente values (1,1,'residenza');
