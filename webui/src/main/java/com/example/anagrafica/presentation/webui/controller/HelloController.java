8fd1952b453373b981e3486603a3f5b04d499aca
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-11-05 17:00:42 +0100

    Completely changed webui module.

---
+++ webui/src/main/java/com/example/anagrafica/presentation/webui/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.anagrafica.presentation.webui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/hello") 
+    public String sayhello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
---
+++ webui/src/test/java/com/example/anagrafica/presentation/webui/WebuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anagrafica.presentation.webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
