4a443ef56ae511631088df4cb597999b1e110c7c
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-11-05 17:19:36 +0100

    Added possibility to use services in webui.

--- webui/src/main/java/com/example/anagrafica/presentation/webui/WebuiApplication.java
+++ webui/src/main/java/com/example/anagrafica/presentation/webui/WebuiApplication.java
@@ -2,7 +2,13 @@ package com.example.anagrafica.presentation.webui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = {"com.example.anagrafica"})
+@EntityScan(basePackages = {"com.example.anagrafica"})
+@EnableJpaRepositories(basePackages = {"com.example.anagrafica"})
 @SpringBootApplication
 public class WebuiApplication {
 
--- webui/src/main/java/com/example/anagrafica/presentation/webui/controller/HelloController.java
+++ webui/src/main/java/com/example/anagrafica/presentation/webui/controller/HelloController.java
@@ -1,15 +1,23 @@
 package com.example.anagrafica.presentation.webui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.anagrafica.business.ClienteService;
+
 @Controller
 public class HelloController {
+	
+	@Autowired
+	ClienteService clienteService;
+	
     @GetMapping("/hello")
     public String index() {
+    	System.out.println(clienteService.get(1));
         return "index";
     }
 
